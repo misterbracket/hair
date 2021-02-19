03cc1e196873d4568a4f66283efbd9ce438ff95d
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-02-19 12:18:33 +0100

    check your face

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ const videoConstraints = {
 function App() {
   return (
     <div className="app">
-      <h1 className="title">Check your Face before your Zoom Call</h1>
-      <p className="text"></p>
+      <div className="title-wrap">
+        <h1 className="title">Check your Face before your Zoom Call</h1>
+        <p className="text">No worries! I am sure you look amazing. 💃🕺 </p>
+      </div>
       <div className="video">
         <Webcam
           className="camera"
