bf4aba44062638614d11d4a12e95afaf014826de
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-02-19 08:12:16 +0100

    Added some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import Webcam from "react-webcam";
-
+const videoConstraints = {
+  facingMode: "user",
+};
 function App() {
   return (
     <div className="app">
       <h1 className="title">Check your Face before your Zoom Call</h1>
       <p className="text"></p>
       <div className="video">
-        <Webcam className="camera" />
+        <Webcam
+          className="camera"
+          audio={false}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+        />
       </div>
     </div>
   );
