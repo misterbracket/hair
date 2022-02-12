3514f2300700e9fb8648ceb1f2f1e6b0de012831
Max Klammer <maximilian.klammer@gmail.com>
2022-02-12 21:26:03 +0100

    improvements

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import Webcam from "react-webcam";
 const videoConstraints = {
   facingMode: "user",
 };
+
 function App() {
   return (
     <div className="app">
-      <div className="title-wrap">
-        <h1 className="title">Check your Face before your Zoom Call</h1>
-        <p className="text">No worries! I am sure you look amazing. 💃🕺 </p>
-      </div>
-      <div className="video">
-        <Webcam
-          className="camera"
-          audio={false}
-          screenshotFormat="image/jpeg"
-          videoConstraints={videoConstraints}
-        />
+      <div className="wrapper">
+        <header className="title-wrap">
+          <h1 className="title">Check your Face</h1>
+        </header>
+        <section className="text-wrapper">
+          <p className="text">No worries! I am sure you look amazing. 💃🕺 </p>
+        </section>
+        <main className="video">
+          <Webcam
+            audio={false}
+            mirrored={true}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+        </main>
       </div>
     </div>
   );
